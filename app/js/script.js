e6dd7d0b5c1ba09da1a6e3a6933effd8f081924a
Lopachenok <lopachenock@yandex.ru>
2016-03-29 13:30:14 +0400

    Styles and scripts for promo slider

--- app/js/script.js
+++ app/js/script.js
@@ -5,10 +5,16 @@ $(document).ready(function(){
   
   $(document).on('click', '.promo-slider__tabs-item a', function(e){
     e.preventDefault();
-    $('.promo-slider__tabs-item--active').removeClass('.promo-slider__tabs-item--active');
+    
+    $('.promo-slider__tabs-item--active').removeClass('promo-slider__tabs-item--active');
     $(this).parent().addClass('promo-slider__tabs-item--active');
+    
     var slideId = $(this).attr('href');
+    
     $('.img-slider__item--active').removeClass('img-slider__item--active');
     $('#'+slideId).addClass('img-slider__item--active');
+    
+    $('.service-slider__item--active').removeClass('service-slider__item--active');
+    $('#service-'+slideId).addClass('service-slider__item--active');
   })
 })
